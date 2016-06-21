94371d54980c7b8bf8e3663d81db3b23d8597372
maestro16 <ilia.busnuk16@gmail.com>
2016-06-21 11:56:23 +0300

    проверка id

--- DataBase/DeleteBD.php
+++ DataBase/DeleteBD.php
@@ -3,13 +3,22 @@ require_once 'inspection.php';
 require_once 'connectBD.php';
 
 $id = $_POST['id'];
-
-
-$sql = "DELETE FROM `u516131903_proj`.`Project` WHERE `id` = $id";
-$result = $conn->prepare($sql);
-$result->bindValue(":id", $id);
-echo $result->execute();
-
-$conn->lastInsertId();
-
+$search_id = "SELECT * FROM `u516131903_proj`.`Project` WHERE `id` = ?";
+$dba = $dbh->prepare($search_id);
+$result = $dba->execute(array($id))or die("ERROR: ".mysql_error());
+$row = $dba->fetch(PDO::FETCH_ASSOC);
+if(!empty($row['id']))
+ {
+ try {
+    $sql = "DELETE FROM `u516131903_proj`.`Project` WHERE `id` = $id";
+    $result = $dbh->query($sql) or die("ERROR: ".mysql_error());
+    print("Запись удалена!");
+      }
+      catch (Exception $e) {
+          print '<br>Ошибка:' . $e->getMessage(). '<br>';
+      }
+    }
+else {
+  print ('id=' . $id . ' не существет!');
+}
 ?>
--- DataBase/insertBD.php
+++ DataBase/insertBD.php
@@ -2,20 +2,28 @@
 require_once 'inspection.php';
 require_once 'connectBD.php';
 
-
-$FIO = $_POST['FIO'];
-$Vuz = $_POST['Vuz'];
-$Course = $_POST['Course'];
-$Phone = $_POST['Phone'];
-
-//$sqlstring = "INSERT INTO `Project`.`u516131903_proj` (id, FIO, Vuz, Course, Phone) VALUES(:id, :FIO, :Vuz, :Course, :Phone)";
-$sql = "INSERT INTO `u516131903_proj`.`Project` (`FIO`, `Vuz`, `Course`, `Phone`) VALUES (:FIO, :Vuz, :Course, :Phone)";
-$result = $conn->prepare($sql);
-$result->bindValue(":FIO", $FIO);
-$result->bindValue(":Vuz", $Vuz);
-$result->bindValue(":Course", $Course);
-$result->bindValue(":Phone", $Phone);
-$message = 'Data saved: ' . $result->execute();
-$conn->lastInsertId();
-
+if(
+!empty($_POST['FIO']) &&
+!empty($_POST['Vuz']) &&
+!empty($_POST['Course']) &&
+!empty($_POST['Phone'])
+)
+ {
+  try {
+    $FIO = trim(stripcslashes(htmlspecialchars($_POST['FIO'])));
+    $Vuz = trim(stripcslashes(htmlspecialchars($_POST['Vuz'])));
+    $Course = trim(stripcslashes(htmlspecialchars($_POST['Course'])));
+    $Phone = trim(stripcslashes(htmlspecialchars($_POST['Phone'])));
+    $sql = "INSERT INTO `u516131903_proj`.`Project` (`FIO`, `Vuz`, `Course`, `Phone`) VALUES (?,?,?,?)";
+    $dba = $dbh->prepare($sql) or die("ERROR: ".mysql_error());
+    $result = $dba->execute(array($FIO,$Vuz,$Course,$Phone))or die("ERROR: ".mysql_error());
+    print('Запись добавлена!');
+  }
+  catch (Exception $e) {
+      echo '<br>Ошибка:' . $e->getMessage(). '<br>';
+  }
+}
+  else{
+    print('Заполните все строки!');
+  }
 ?>
