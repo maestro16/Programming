e0e675931cfdf4c59a7e053a42ea73e2044060a6
maestro16 <ilia.busnuk16@gmail.com>
2016-06-21 10:29:17 +0300

    проверка id

--- DataBase/DeleteBD.php
+++ DataBase/DeleteBD.php
@@ -4,14 +4,12 @@ require_once 'connectBD.php';
 
 $id = $_POST['id'];
 
- try {
-    $sql = "DELETE FROM `u516131903_proj`.`Project` WHERE `id` = $id";
-    $result = $dbh->prepare($sql) or die("ERROR: ".mysql_error());
-    print("Запись удалена!");
-    $result->bindValue(":id", $id);
-    $result->execute();
-      }
-      catch (Exception $e) {
-          print '<br>Ошибка:' . $e->getMessage(). '<br>';
-      }
+
+$sql = "DELETE FROM `u516131903_proj`.`Project` WHERE `id` = $id";
+$result = $conn->prepare($sql);
+$result->bindValue(":id", $id);
+echo $result->execute();
+
+$conn->lastInsertId();
+
 ?>
